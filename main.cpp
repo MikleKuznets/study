3ffc4477dbc593a06eac20323b04a4e669833239
MikleKuznets <99048142+MikleKuznets@users.noreply.github.com>
2022-02-26 11:33:20 +0300

    dz4_4

--- main.cpp
+++ main.cpp
@@ -1,48 +1,49 @@
-//   Дана матрица 7х7, поменять местами К-й столбец с К-ой строкой
+// вычислить сумму элементов матрицы слева от побочной диагонали
 
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
 
 
 int main()
 {
-    constexpr int size = 7;
-    int M[7][7];
-    int mirrow [7][7];
-    int k;
+    int size = 10;
+    int M[size][size];
+    int sum, count;
 
     srand(time(0));
 
-    for (int a = 0; a < size; ++a)
-    {
-        for (int b = 0; b < size; ++b)
-        {
-            M[b][a] = rand() % 10;
-            printf ("%d ", M[b][a]);
-        }
-        printf ("\n");
-    }
-
-    printf ("Enter k (1 - 7) = ");
-    scanf ("%d", &k);
+    printf ("Enter massive = ");
+    scanf ("%d", &size);
     printf ("\n");
 
     for (int a = 0; a < size; ++a)
     {
-        mirrow [k-1][a] = M [a][k-1];
-        mirrow [a][k-1] = M [k-1][a];
-    }
-
-    for (int a = 0; a < size; ++a)
-    {
         for (int b = 0; b < size; ++b)
         {
-            M [b][k-1] = mirrow [b][k-1];
-            M [k-1][a] = mirrow [k-1][a];
-            printf ("%d ", M[b][a]);
+            M[a][b] = rand() % 10;
+            printf ("%d ", M[a][b]);
         }
         printf ("\n");
     }
+
+    printf ("\n");
+    count = size - 1;
+    sum = 0;
+    printf ("New massiv = \n");
+
+    for (int a = 0; a < size; ++a)
+    {
+        for (int b = 0; b < count; ++b)
+        {
+            sum = sum + M[a][b];
+            printf ("%d ", M[a][b]);
+        }
+        printf ("\n");
+        count = count - 1;
+    }
+
+    printf("summ = %d\n", sum);
+
     return 0;
 }
