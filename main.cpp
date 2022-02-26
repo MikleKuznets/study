ce9a203e1ff0f497e9ded38f01f2adb71c043dcb
MikleKuznets <99048142+MikleKuznets@users.noreply.github.com>
2022-02-26 12:31:13 +0300

    dz2_1

--- main.cpp
+++ main.cpp
@@ -1,16 +1,61 @@
 #include <stdio.h>
 #include <math.h>
-#include <string.h>
 
 int main()
+
 {
+    float x;
+    float y;
+    float a; // переменная введена, чтобы разбить уравнение - верх //
+    float b, c; // переменная введена, чтобы разбить уравнение - низ //
+    float d; // переменная введена, чтобы разбить уравнение - е в кубе//
 
-    unsigned long long X;
-    int count=0;
-    char number [50];
+    printf ("x=");
+    scanf ("%f", &x);
 
-    printf ("Enter X = ");
-    scanf ("%ull", &X);
+    // верхняя часть уравнения //
+
+    a = M_PI / 6;
+    a = x - a;
+    a = cos (a);
+    a = 2 * a + sqrt(2);
+
+    //  printf ("up= %f\n",a);
+
+    // нижняя часть уравнения //
+
+    b = pow (x, 2);
+    b = sin (b);
+    b = pow (b, 2);
+
+    //  printf ("down1= %f\n", b);
+
+    c = log(x);
+
+    //  printf ("down_log= %f\n", c);
+
+    c = 2 * c;
+    c = 1 / c;
+
+    //  printf ("down2= %f\n", c);
+
+    b = b + c;
+
+    //  printf ("down= %f\n", b);
+
+    // е в кубе //
+
+    d = 3 * x;
+    d = exp (d);
+
+    //  printf ("exp= %f\n", d);
+
+    // финал //
+
+    y = a / b * d;
+
+    printf ("y= %f\n", y);
+
 
     return 0;
 }
