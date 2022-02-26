39fb6f8070b1a53e4e34284b99d3eadf1ab64684
MikleKuznets <99048142+MikleKuznets@users.noreply.github.com>
2022-02-26 12:02:58 +0300

    dz3_5

--- main.cpp
+++ main.cpp
@@ -3,38 +3,14 @@
 #include <string.h>
 
 int main()
-
 {
-unsigned long long N;
-int count = 0;
-char number [50];
 
-printf ("Enter N = ");
-scanf ("%llu", &N);
-//  printf ("%llu", N);
+    unsigned long long X;
+    int count=0;
+    char number [50];
 
-do
-{
-    if (N % 2)
-    {
-        number [count] = '1';
-        printf ("%c", number[count]);
-    }
-    else
-    {
-        number [count] = '0';
-        printf ("%c", number[count]);
-    }
-
-    count = count + 1;
-    N = N / 2;
-}
-while (N);
-number [count] = '\0';
-for (int i = count-- ; i > 0 ; i--)
-{
-        printf ("%c", number[i-1]);
-}
+    printf ("Enter X = ");
+    scanf ("%ull", &X);
 
     return 0;
 }
