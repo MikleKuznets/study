20111ea09bd8aa7adfa053a6fe057fe5df9545ab
MikleKuznets <99048142+MikleKuznets@users.noreply.github.com>
2022-03-11 13:26:13 +0300

    dz1_2

--- main.cpp
+++ main.cpp
@@ -1,37 +1,35 @@
 #include <stdio.h>
 int main(void)
-{
-    int a, b = 5, c;
-    float x, y = -.5, z;
-    printf ("a=");
-    scanf("%d", &a);
-    x = c = a;
-    printf("a=%d, c=%d, x=%f\n", a, c, x);
-    a+= b;
-    printf("a=%d\n",a);
-    x+= b + a;
-    printf("x=%f\n", x);
-    b+=a--;
-    printf("b=%d\n", b);
-    x-=++c;
-    printf("x=%f\n",x);
-    c=a/b;
-    printf("c=%4d\n", c);
-    c=a%b;
-    printf("c=%d\n", c);
-    x=5.3;
-    y+=((--x-1)/x++);
-    printf("x=%f\ty=%.2f\n\n x=%.0f\ty=%.of\n", x-1, y, x, y-1);
-    z=a/2;
-    printf("z=%f\n",z);
-    z=(float)a/2;
-    printf("z=%f\n", z);
-    y=x/2;
-    printf("y=%f\n", y);
-    y=(int)x/2;
-    printf("y=%f\n", y);
-    z=a%2-(x+b)/c+(x-y)/(a-1)+1/4*a-y++ + ++b/3.;
-    printf("a=%d b=%d c=%d x=%f y=%f z=%f\n", a, b, c, x, y, z);
-    return 0;
 
+{
+    int a = 5;
+    int b;
+    int c = 5;
+    printf("a=%d\n", a);
+    printf("c=%d\n", c);
+    printf("b=");
+    scanf("%d", &b);
+    a = a + b - 2;
+    printf("a=%d\n", a);
+    c = c + 1;
+    printf("c=%d\n", c);
+    int d;
+    printf("d=");
+    scanf("%d", &d);
+    d = c - a + d;
+    printf("d=%d\n", d);
+    a = a * c;
+    printf("a=%d\n", a);
+    c = c - 1;
+    printf("c=%d\n", c);
+    a = a/10;
+    printf("a=%d\n", a);
+    c = c/2;
+    printf("c=%d\n", c);
+    b = b - 1;
+    printf("b=%d\n", b);
+    d = d * (c + b + a);
+    printf("d=%d\n", d);
+
+    return 0;
 }
